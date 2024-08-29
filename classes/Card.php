98bbb570e70037cd289da3074dc0778fa748c5de
lucas-iribaren <lucas.iribaren@laplateforme.io>
2024-08-29 13:59:21 +0200

    feat(Card) ajout de la classe Card pour récupérer informations
    Ajout dans le dossier 'classes/'

---
+++ classes/Card.php
@@ -0,0 +1,38 @@
+<?php
+// classes/Card.php
+
+class Card {
+    private $id;
+    private $image;
+    private $isMatched = false;
+
+    public function __construct($id, $image) {
+        $this->id = $id;
+        $this->image = $image;
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getImage() {
+        return $this->image;
+    }
+
+    public function isMatched() {
+        return $this->isMatched;
+    }
+
+    public function setMatched($isMatched) {
+        $this->isMatched = $isMatched;
+    }
+
+    public function flip() {
+        // Logique pour retourner la carte (afficher ou cacher)
+    }
+
+    public function checkMatch($otherCard) {
+        return $this->id === $otherCard->getId();
+    }
+}
+?>
