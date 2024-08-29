b751cfb63e6d41fcdec66d2e0111520741625964
lucas-iribaren <lucas.iribaren@laplateforme.io>
2024-08-29 14:59:00 +0200

    feat(leaderboard): ajouter la classe Leaderboard pour gérer le classement des joueurs
    Ajout de la classe `Leaderboard` pour suivre et mettre à jour le classement des meilleurs joueurs :
    - Initialisation du classement à partir d'un fichier JSON.
    - Mise à jour et sauvegarde du classement avec les scores des joueurs.

---
+++ classes/Leaderboard.php
@@ -0,0 +1,42 @@
+<?php
+// classes/Leaderboard.php
+
+class Leaderboard {
+    private $topPlayers = [];
+
+    public function __construct() {
+        $this->initializeLeaderboard();
+        $this->loadLeaderboard();
+    }
+
+    private function initializeLeaderboard() {
+        $filename = 'data/leaderboard.json';
+        if (!file_exists($filename)) {
+            file_put_contents($filename, json_encode([]));
+        }
+    }
+
+    private function loadLeaderboard() {
+        $filename = 'data/leaderboard.json';
+        if (file_exists($filename)) {
+            $this->topPlayers = json_decode(file_get_contents($filename), true);
+        }
+    }
+
+    public function updateLeaderboard(Player $player) {
+        $this->topPlayers[$player->getName()] = $player->getBestScore();
+        arsort($this->topPlayers);
+        $this->topPlayers = array_slice($this->topPlayers, 0, 10);
+        $this->saveLeaderboard();
+    }
+
+    private function saveLeaderboard() {
+        $filename = 'data/leaderboard.json';
+        file_put_contents($filename, json_encode($this->topPlayers));
+    }
+
+    public function getTopPlayers() {
+        return $this->topPlayers;
+    }
+}
+?>
